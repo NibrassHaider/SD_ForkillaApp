48c11364b7ff71eb3deda5250fa77d1419c4530a
NibrassHaider <nhaideha7@alumnes.ub.edu>
2019-05-30 17:03:20 +0200

    Update models.py

--- forkilla/models.py
+++ forkilla/models.py
@@ -58,7 +58,7 @@ class Restaurant(models.Model):
         city = models.CharField(max_length=50)
         country = models.CharField(max_length=50)
         #featured_photo = models.ImageField()
-        featured_photo = models.CharField(max_length=250)
+        featured_photo = models.CharField(max_length=90)
         category = models.CharField(max_length=5, choices=CATEGORIES)
         capacity = models.PositiveIntegerField()
 
